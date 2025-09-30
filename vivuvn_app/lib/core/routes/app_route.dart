78a6ef89114ac9e442148116665f3291909890d7
diendanx <urekkmazinno@gmail.com>
2025-09-30 15:05:12 +0700

    Add bottom navigation bar for client app

--- vivuvn_app/lib/core/routes/app_route.dart
+++ vivuvn_app/lib/core/routes/app_route.dart
@@ -2,8 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
+import '../../screens/bottom_navigation_screen.dart';
 import '../../screens/home_screen.dart';
+import '../../screens/itinerary_screen.dart';
+import '../../screens/profile_screen.dart';
 import '../../screens/route_error_screen.dart';
+import '../../screens/search_screen.dart';
 import 'routes.dart';
 
 final goRouterProvider = Provider<GoRouter>((final ref) {
@@ -15,14 +19,63 @@ final goRouterProvider = Provider<GoRouter>((final ref) {
     ),
     routes: <RouteBase>[
       GoRoute(
-        path: homeRoute,
-        builder: (final context, final state) => const HomeScreen(),
-      ),
-      GoRoute(
         path: loginRoute,
         builder: (final context, final state) {
           return const Scaffold(body: Center(child: Text('Login Page')));
         },
+      ),
+
+      // Route with Bottom Navigation
+      StatefulShellRoute.indexedStack(
+        builder:
+            (
+              final BuildContext context,
+              final GoRouterState state,
+              final StatefulNavigationShell navigationShell,
+            ) => BottomNavigationScreen(navigationShell: navigationShell),
+
+        branches: <StatefulShellBranch>[
+          // Home Branch
+          StatefulShellBranch(
+            routes: <RouteBase>[
+              GoRoute(
+                path: homeRoute,
+                builder: (final context, final state) => const HomeScreen(),
+              ),
+            ],
+          ),
+
+          // Search Branch
+          StatefulShellBranch(
+            routes: <RouteBase>[
+              GoRoute(
+                path: searchRoute,
+                builder: (final context, final state) => const SearchScreen(),
+              ),
+            ],
+          ),
+
+          // Itinerary Branch
+          StatefulShellBranch(
+            routes: <RouteBase>[
+              GoRoute(
+                path: itineraryRoute,
+                builder: (final context, final state) =>
+                    const ItineraryScreen(),
+              ),
+            ],
+          ),
+
+          // Profile Branch
+          StatefulShellBranch(
+            routes: <RouteBase>[
+              GoRoute(
+                path: profileRoute,
+                builder: (final context, final state) => const ProfileScreen(),
+              ),
+            ],
+          ),
+        ],
       ),
     ],
   );
--- vivuvn_app/lib/core/routes/routes.dart
+++ vivuvn_app/lib/core/routes/routes.dart
@@ -1,4 +1,7 @@
 const String loginRoute = '/login';
 const String registerRoute = '/register';
-const String profileRoute = '/profile';
+
 const String homeRoute = '/';
+const String profileRoute = '/profile';
+const String itineraryRoute = '/itinerary';
+const String searchRoute = '/search';
