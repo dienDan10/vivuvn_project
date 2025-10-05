438495c387279f3a444adee97f2c89b89c9f820d
SinhThai18 <156230586+NguyenSinhThai18@users.noreply.github.com>
2025-10-05 22:49:45 +0700

    using dio excep in register

--- vivuvn_app/lib/features/register/controller/register_controller.dart
+++ vivuvn_app/lib/features/register/controller/register_controller.dart
@@ -1,6 +1,7 @@
-import 'dart:convert';
 import 'package:dio/dio.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../../../core/data/remote/exception/dio_exception_handler.dart';
 import '../data/dto/register_request.dart';
 import '../service/register_service.dart';
 import '../state/register_state.dart';
@@ -28,35 +29,16 @@ class RegisterController extends AutoDisposeNotifier<RegisterState> {
       state = state.copyWith(isLoading: false);
       return true;
     } on DioException catch (e) {
-      String errorMsg = 'Unknown error';
-      String? errorDetail;
-
-      if (e.response != null && e.response?.data != null) {
-        var data = e.response?.data;
-
-        if (data is String) {
-          try {
-            data = jsonDecode(data);
-          } catch (_) {}
-        }
-
-        if (data is Map<String, dynamic>) {
-          errorDetail = data['detail'] as String?;
-          errorMsg = errorDetail ?? data['title'] ?? errorMsg;
-        } else if (data is String) {
-          errorMsg = data;
-        }
-      } else {
-        errorMsg = e.message ?? errorMsg;
-      }
-
-      state = state.copyWith(error: errorDetail ?? errorMsg, isLoading: false);
-
+      final errorMsg = DioExceptionHandler.handleException(e);
+      state = state.copyWith(error: errorMsg, isLoading: false);
+      return false;
+    } catch (e) {
+      state = state.copyWith(error: e.toString(), isLoading: false);
       return false;
     }
   }
 
-  /// Cập nhật cả 3 trường
+  /// Cập nhật tất cả trường cùng lúc
   void updateRegisterData(
     final String email,
     final String username,
