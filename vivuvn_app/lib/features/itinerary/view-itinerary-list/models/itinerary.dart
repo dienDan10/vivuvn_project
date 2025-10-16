29951a307ad7c89a86f997a45afe443da85ca5f2
SinhThai18 <156230586+NguyenSinhThai18@users.noreply.github.com>
2025-10-17 00:45:06 +0700

    call api get itinerary by user done

--- vivuvn_app/lib/features/itinerary/view-itinerary-list/models/itinerary.dart
+++ vivuvn_app/lib/features/itinerary/view-itinerary-list/models/itinerary.dart
@@ -1,5 +1,5 @@
 class Itinerary {
-  final String id;
+  final int id;
   final String name;
   final String imageUrl;
   final DateTime startDate;
@@ -12,31 +12,14 @@ class Itinerary {
     required this.startDate,
     required this.endDate,
   });
-}
 
-final List<Itinerary> dummyItineraries = [
-  Itinerary(
-    id: '1',
-    name: 'Trip to Paris',
-    imageUrl:
-        'https://plus.unsplash.com/premium_photo-1738856608580-23d0ab1632e4?q=80&w=1550&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    startDate: DateTime(2023, 10, 1),
-    endDate: DateTime(2023, 10, 7),
-  ),
-  Itinerary(
-    id: '2',
-    name: 'Beach Vacation',
-    imageUrl:
-        'https://images.unsplash.com/photo-1759691397755-9f761d9615ec?q=80&w=1742&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    startDate: DateTime(2023, 11, 15),
-    endDate: DateTime(2023, 11, 20),
-  ),
-  Itinerary(
-    id: '3',
-    name: 'Mountain Hiking',
-    imageUrl:
-        'https://images.unsplash.com/photo-1733169312700-a3dfad2d99f0?q=80&w=1742&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    startDate: DateTime(2024, 1, 5),
-    endDate: DateTime(2024, 1, 12),
-  ),
-];
+  factory Itinerary.fromJson(final Map<String, dynamic> json) {
+    return Itinerary(
+      id: json['id'] as int,
+      name: json['name'] ?? '',
+      imageUrl: json['imageUrl'] ?? 'assets/images/images-placeholder.jpeg',
+      startDate: DateTime.parse(json['startDate'] as String),
+      endDate: DateTime.parse(json['endDate'] as String),
+    );
+  }
+}
--- vivuvn_app/lib/features/itinerary/view-itinerary-list/ui/widgets/itinerary_list.dart
+++ vivuvn_app/lib/features/itinerary/view-itinerary-list/ui/widgets/itinerary_list.dart
@@ -1,17 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import '../../models/itinerary.dart';
+import '../../controller/itinerary_controller.dart';
+import 'EmptyItinerary.dart';
 import 'itinerary_list_item.dart';
 
-class ItineraryList extends StatelessWidget {
+class ItineraryList extends ConsumerStatefulWidget {
   const ItineraryList({super.key});
 
   @override
+  ConsumerState<ItineraryList> createState() => _ItineraryListState();
+}
+
+class _ItineraryListState extends ConsumerState<ItineraryList> {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      ref.read(itineraryControllerProvider.notifier).fetchItineraries();
+    });
+  }
+
+  @override
   Widget build(final BuildContext context) {
+    final state = ref.watch(itineraryControllerProvider);
+
+    if (state.isLoading) {
+      return const Center(child: CircularProgressIndicator());
+    }
+
+    if (state.error != null) {
+      return Center(child: Text('Error: ${state.error}'));
+    }
+
+    if (state.itineraries.isEmpty) {
+      return const EmptyItinerary();
+    }
+
     return ListView.builder(
-      itemCount: dummyItineraries.length,
+      itemCount: state.itineraries.length,
       itemBuilder: (final ctx, final index) {
-        return ItineraryListItem(itinerary: dummyItineraries[index]);
+        return ItineraryListItem(itinerary: state.itineraries[index]);
       },
     );
   }
