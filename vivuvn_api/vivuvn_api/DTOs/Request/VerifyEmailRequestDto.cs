5e7de7769a6513b075c90df6103a6b2e45ae0753
diendanx <urekkmazinno@gmail.com>
2025-10-18 18:06:21 +0700

    add endpoint for verify email when register

---
+++ vivuvn_api/vivuvn_api/DTOs/Request/VerifyEmailRequestDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace vivuvn_api.DTOs.Request
+{
+    public class VerifyEmailRequestDto
+    {
+        [Required]
+        public string Email { get; set; } = null!;
+        [Required]
+        public string Token { get; set; } = null!;
+    }
+}
--- vivuvn_api/vivuvn_api/Services/Implementations/AuthService.cs
+++ vivuvn_api/vivuvn_api/Services/Implementations/AuthService.cs
@@ -9,7 +9,7 @@ using vivuvn_api.Services.Interfaces;
 
 namespace vivuvn_api.Services.Implementations
 {
-    public class AuthService(AppDbContext _context, ITokenService _tokenService) : IAuthService
+    public class AuthService(AppDbContext _context, ITokenService _tokenService, IEmailService _emailService) : IAuthService
     {
         public async Task<TokenResponseDto> LoginAsync(LoginRequestDto request)
         {
@@ -18,7 +18,7 @@ namespace vivuvn_api.Services.Implementations
                 .ThenInclude(ur => ur.Role)
                 .FirstOrDefaultAsync(u => u.Email == request.Email);
 
-            if (user == null) throw new BadHttpRequestException("User not found");
+            if (user == null) throw new BadHttpRequestException("Email or Password incorrect");
 
             var passwordVerificationResult = new PasswordHasher<User>().VerifyHashedPassword(user, user.PasswordHash, request.Password);
 
@@ -33,25 +33,32 @@ namespace vivuvn_api.Services.Implementations
             return await CreateTokenResponse(user);
         }
 
-        public async Task<RegisterResponseDto> RegisterAsync(RegisterRequestDto request)
+        public async Task RegisterAsync(RegisterRequestDto request)
         {
-            if (await _context.Users.AnyAsync(u => u.Email == request.Email))
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.Username == request.Username);
+
+            if (user is not null && user.IsEmailVerified)
+            {
+                // Send verification email again
+                user.Username = request.Username;
+                user.PasswordHash = HashPassword(user, request.Password);
+                await CreateAndSendEmailVerificationToken(user);
+                return;
+            }
+
+            if (user is not null)
             {
                 throw new BadHttpRequestException("Email is already in use");
             }
 
-            var user = new User
+            var newUser = new User
             {
                 Email = request.Email,
                 Username = request.Username,
-                IsEmailVerified = true, // Let the email always be valid for now
+                IsEmailVerified = false,
             };
 
-            var hashedPassword = new PasswordHasher<User>().HashPassword(user, request.Password);
-            user.PasswordHash = hashedPassword;
-
-            user.EmailVerificationToken = _tokenService.CreateEmailVerificationToken();
-            user.EmailVerificationTokenExpireDate = DateTime.UtcNow.AddMinutes(Constants.EmailVerificationTokenExpirationMinutes);
+            newUser.PasswordHash = HashPassword(newUser, request.Password);
 
             // Assign "Traveler" role by default
             var travelerRole = await _context.Roles.FirstOrDefaultAsync(r => r.Name == Constants.Role_Traveler);
@@ -61,20 +68,41 @@ namespace vivuvn_api.Services.Implementations
                 throw new Exception("An unexpected error has occurred");
             }
 
-            user.UserRoles = new List<UserRole> { new UserRole { RoleId = travelerRole.Id } };
+            newUser.UserRoles = new List<UserRole> { new UserRole { RoleId = travelerRole.Id } };
 
-            // Send Email to User
 
             // Add user to db
-            await _context.Users.AddAsync(user);
+            await _context.Users.AddAsync(newUser);
             await _context.SaveChangesAsync();
 
-            return new RegisterResponseDto
+            // Send Email to User
+            await CreateAndSendEmailVerificationToken(newUser);
+
+            return;
+        }
+
+        public async Task VerifyEmailAsync(VerifyEmailRequestDto request)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.Email == request.Email);
+            if (user == null) throw new BadHttpRequestException("User not found");
+            if (user.IsEmailVerified) return;
+            if (user.EmailVerificationToken != request.Token || user.EmailVerificationTokenExpireDate <= DateTime.UtcNow)
             {
-                Id = user.Id.ToString(),
-                Email = user.Email,
-                Username = user.Username
-            }; 
+                throw new BadHttpRequestException("Invalid or expired verification token");
+            }
+            user.IsEmailVerified = true;
+            user.EmailVerificationToken = null;
+            user.EmailVerificationTokenExpireDate = null;
+            _context.Users.Update(user);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task ResendEmailVerificationAsync(ResendEmailVerificationRequestDto request)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.Email == request.Email);
+            if (user == null) throw new BadHttpRequestException("User not found");
+            if (user.IsEmailVerified) return;
+            await CreateAndSendEmailVerificationToken(user);
         }
 
         public async Task<TokenResponseDto> RefreshTokenAsync(RefreshTokenRequestDto request)
@@ -84,6 +112,21 @@ namespace vivuvn_api.Services.Implementations
             if (user == null) throw new BadHttpRequestException("Invalid refresh token");
 
             return await CreateTokenResponse(user);
+        }
+
+        private string HashPassword(User user, string password)
+        {
+            return new PasswordHasher<User>().HashPassword(user, password);
+        }
+
+        private async Task CreateAndSendEmailVerificationToken(User user)
+        {
+            user.EmailVerificationToken = _tokenService.CreateEmailVerificationToken();
+            user.EmailVerificationTokenExpireDate = DateTime.UtcNow.AddMinutes(Constants.EmailVerificationTokenExpirationMinutes);
+            _context.Users.Update(user);
+            await _context.SaveChangesAsync();
+            // Send Email to User
+            await _emailService.SendEmailAsync(user.Email, "Email Verification", $"Your verification token is {user.EmailVerificationToken}");
         }
 
         private async Task<TokenResponseDto> CreateTokenResponse(User user)
