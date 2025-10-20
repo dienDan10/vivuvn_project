c49954f274a6829cc0d79945d2cb3ea47826ac01
diendanx <urekkmazinno@gmail.com>
2025-10-20 23:47:41 +0700

    fix budgettype dto not mapping to Id

--- vivuvn_api/vivuvn_api/DTOs/ValueObjects/BudgetTypeDto.cs
+++ vivuvn_api/vivuvn_api/DTOs/ValueObjects/BudgetTypeDto.cs
@@ -2,7 +2,7 @@
 {
     public class BudgetTypeDto
     {
-        public int Id { get; set; }
+        public int BudgetTypeId { get; set; }
         public string Name { get; set; } = string.Empty;
     }
 }
