6b62d60e175e6790a83efa48ce8be8488b0482df
diendanx <urekkmazinno@gmail.com>
2025-10-21 18:25:12 +0700

    add endpoint for get locations with pagination, sort and filter

--- vivuvn_api/vivuvn_api/Controllers/ProvinceController.cs
+++ vivuvn_api/vivuvn_api/Controllers/ProvinceController.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using vivuvn_api.DTOs.Request;
-using vivuvn_api.DTOs.ValueObjects;
 using vivuvn_api.Services.Interfaces;
 
 namespace vivuvn_api.Controllers
@@ -14,57 +13,56 @@ namespace vivuvn_api.Controllers
         public async Task<IActionResult> SearchProvinces([FromQuery] SearchProvinceRequestDto requestDto)
         {
 
-            // Dummy data for demonstration purposes
             var provinces = await _provinceService.SearchProvinceAsync(requestDto.Name, requestDto.Limit);
             return Ok(provinces);
         }
 
         [HttpGet]
         [Authorize(Roles = "Admin")]
-		public async Task<IActionResult> GetAllProvinces([FromQuery] GetAllProvincesRequestDto requestDto)
+        public async Task<IActionResult> GetAllProvinces([FromQuery] GetAllProvincesRequestDto requestDto)
         {
             var provinces = await _provinceService.GetAllProvincesAsync(requestDto);
             return Ok(provinces);
-		}
+        }
 
-		[HttpPut("{id:int}/restore")]
-		[Authorize(Roles = "Admin")]
-		public async Task<IActionResult> RestoreProvice(int id)
-		{
-			var provinces = await _provinceService.RestoreProvinceAsync(id);
-			return Ok(provinces);
-		}
+        [HttpPut("{id:int}/restore")]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> RestoreProvice(int id)
+        {
+            var provinces = await _provinceService.RestoreProvinceAsync(id);
+            return Ok(provinces);
+        }
 
-		[HttpDelete("{id:int}")]
-		[Authorize(Roles = "Admin")]
-		public async Task<IActionResult> DeleteProvice(int id)
-		{
-			await _provinceService.DeleteProvinceAsync(id);
-			return Ok();
-		}
+        [HttpDelete("{id:int}")]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> DeleteProvice(int id)
+        {
+            await _provinceService.DeleteProvinceAsync(id);
+            return Ok();
+        }
 
-		[HttpGet("{id:int}")]
-		public async Task<IActionResult> GetProvinceById(int id)
-		{
-			var province = await _provinceService.GetProvinceByIdAsync(id);
-			return Ok(province);
-		}
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetProvinceById(int id)
+        {
+            var province = await _provinceService.GetProvinceByIdAsync(id);
+            return Ok(province);
+        }
 
-		[HttpPost]
-		[Authorize(Roles = "Admin")]
-		public async Task<IActionResult> CreateProvince([FromForm] CreateProvinceRequestDto requestDto)
-		{
-			var province = await _provinceService.CreateProvinceAsync(requestDto);
-			return CreatedAtAction(nameof(GetProvinceById), new { id = province.Id }, province);
-		}
+        [HttpPost]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> CreateProvince([FromForm] CreateProvinceRequestDto requestDto)
+        {
+            var province = await _provinceService.CreateProvinceAsync(requestDto);
+            return CreatedAtAction(nameof(GetProvinceById), new { id = province.Id }, province);
+        }
 
-		[HttpPut("{id:int}")]
-		[Authorize(Roles = "Admin")]
-		public async Task<IActionResult> UpdateProvince(int id, [FromForm] UpdateProvinceRequestDto requestDto)
-		{
-			var province = await _provinceService.UpdateProvinceAsync(id, requestDto);
-			return Ok(province);
-		}
+        [HttpPut("{id:int}")]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> UpdateProvince(int id, [FromForm] UpdateProvinceRequestDto requestDto)
+        {
+            var province = await _provinceService.UpdateProvinceAsync(id, requestDto);
+            return Ok(province);
+        }
 
-	}
+    }
 }
