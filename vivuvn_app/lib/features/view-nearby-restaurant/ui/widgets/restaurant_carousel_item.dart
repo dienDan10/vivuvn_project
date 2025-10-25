55d2d1f963da9bb9cc6887d748969e9dc0b21b0c
diendanx <urekkmazinno@gmail.com>
2025-10-26 00:53:07 +0700

    add detail ui for restaurant carousel item

--- vivuvn_app/lib/features/view-nearby-restaurant/ui/widgets/restaurant_carousel_item.dart
+++ vivuvn_app/lib/features/view-nearby-restaurant/ui/widgets/restaurant_carousel_item.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 import '../../model/restaurant.dart';
+import 'btn_open_map.dart';
 import 'restaurant_detail_modal.dart';
 
 class RestaurantCarouselItem extends StatelessWidget {
@@ -38,11 +40,124 @@ class RestaurantCarouselItem extends StatelessWidget {
         ),
         child: Column(
           children: [
-            Text(
-              restaurant.name,
-              style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            // Main info row
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                // Restaurant Image
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(12),
+                  child: FadeInImage.assetNetwork(
+                    placeholder: 'assets/images/image-placeholder.jpeg',
+                    image: restaurant.photos.isNotEmpty
+                        ? restaurant.photos[0]
+                        : 'assets/images/image-placeholder.jpeg',
+                    width: 110,
+                    height: 110,
+                    fit: BoxFit.cover,
+                    imageErrorBuilder:
+                        (final context, final error, final stackTrace) {
+                          return Image.asset(
+                            'assets/images/image-placeholder.jpeg',
+                            width: 110,
+                            height: 110,
+                            fit: BoxFit.cover,
+                          );
+                        },
+                  ),
+                ),
+                const SizedBox(width: 12),
+
+                // Restaurant Info
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        restaurant.name,
+                        style: const TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                      const SizedBox(height: 8),
+
+                      // Rating row
+                      Row(
+                        children: [
+                          // Star Icon
+                          const Icon(
+                            Icons.star,
+                            color: Colors.orangeAccent,
+                            size: 16,
+                          ),
+                          const SizedBox(width: 4),
+                          // Rating Text
+                          Text(
+                            '${restaurant.rating}',
+                            style: const TextStyle(
+                              fontSize: 14,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.orangeAccent,
+                            ),
+                          ),
+                          // Rating count
+                          const SizedBox(width: 4),
+                          Text('(${restaurant.userRatingCount})'),
+
+                          // google image
+                          const SizedBox(width: 8),
+                          SvgPicture.asset(
+                            'assets/images/google.svg',
+                            width: 16,
+                            height: 16,
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 8),
+
+                      // Address row
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(top: 4.0),
+                            child: SvgPicture.asset(
+                              'assets/icons/restaurant.svg',
+                              width: 16,
+                              height: 16,
+                            ),
+                          ),
+                          const SizedBox(width: 4),
+                          Expanded(
+                            child: Text(
+                              restaurant.address,
+                              style: const TextStyle(fontSize: 14),
+                              maxLines: 3,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
-            const SizedBox(height: 8),
+
+            // Buttons Groups
+            Expanded(
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  // Map Button
+                  ButtonOpenMap(mapUrl: restaurant.googleMapsUri ?? ''),
+                ],
+              ),
+            ),
           ],
         ),
       ),
