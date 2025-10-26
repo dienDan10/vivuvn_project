4083fb4f6295d94cfcc583ed2a8e9621e88ef7d0
diendanx <urekkmazinno@gmail.com>
2025-10-26 22:25:46 +0700

    add endpoint for add restaurant to itinerary by suggestion

--- vivuvn_api/vivuvn_api/DTOs/Request/AddRestaurantToItineraryFromSuggestionDto.cs
+++ vivuvn_api/vivuvn_api/DTOs/Request/AddRestaurantToItineraryFromSuggestionDto.cs
@@ -5,6 +5,6 @@ namespace vivuvn_api.DTOs.Request
     public class AddRestaurantToItineraryFromSuggestionDto
     {
         [Required]
-        public int HotelId { get; set; }
+        public int RestaurantId { get; set; }
     }
 }
--- vivuvn_api/vivuvn_api/Services/Implementations/ItineraryRestaurantService.cs
+++ vivuvn_api/vivuvn_api/Services/Implementations/ItineraryRestaurantService.cs
@@ -1,4 +1,5 @@
 ﻿using vivuvn_api.DTOs.Request;
+using vivuvn_api.Models;
 using vivuvn_api.Repositories.Interfaces;
 using vivuvn_api.Services.Interfaces;
 
@@ -6,9 +7,19 @@ namespace vivuvn_api.Services.Implementations
 {
     public class ItineraryRestaurantService(IUnitOfWork _unitOfWork) : IitineraryRestaurantService
     {
-        public Task AddRestaurantToItineraryFromSuggestionAsync(int itineraryId, AddRestaurantToItineraryFromSuggestionDto request)
+        public async Task AddRestaurantToItineraryFromSuggestionAsync(int itineraryId, AddRestaurantToItineraryFromSuggestionDto request)
         {
-            throw new NotImplementedException();
+            var itinerary = _unitOfWork.Itineraries.GetOneAsync(i => i.Id == itineraryId)
+                ?? throw new BadHttpRequestException("Itinerary not found");
+
+            var itineraryRestaurant = new ItineraryRestaurant
+            {
+                ItineraryId = itineraryId,
+                RestaurantId = request.RestaurantId
+            };
+
+            await _unitOfWork.ItineraryRestaurants.AddAsync(itineraryRestaurant);
+            await _unitOfWork.SaveChangesAsync();
         }
     }
 }
