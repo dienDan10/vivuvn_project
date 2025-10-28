98a6b2591f3b468dc9dfb8af744015e2e4d6059c
diendanx <urekkmazinno@gmail.com>
2025-10-28 15:37:52 +0700

    add endpoint for get all hotels in itinerary

---
+++ vivuvn_api/vivuvn_api/Controllers/ItineraryHotelController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using vivuvn_api.Services.Interfaces;
+
+namespace vivuvn_api.Controllers
+{
+    [Route("api/v1/itineraries/{itineraryId}/hotels")]
+    [ApiController]
+    public class ItineraryHotelController(IitineraryHotelService _itineraryHotelService) : ControllerBase
+    {
+        [HttpGet]
+        [Authorize]
+        public async Task<IActionResult> GetRestaurantsInItinerary(int itineraryId)
+        {
+            var restaurants = await _itineraryHotelService.GetHotelsInItineraryAsync(itineraryId);
+            return Ok(restaurants);
+        }
+    }
+}
--- vivuvn_api/vivuvn_api/Services/Implementations/ItineraryHotelService.cs
+++ vivuvn_api/vivuvn_api/Services/Implementations/ItineraryHotelService.cs
@@ -1,8 +1,17 @@
-﻿using vivuvn_api.Services.Interfaces;
+﻿using AutoMapper;
+using vivuvn_api.DTOs.ValueObjects;
+using vivuvn_api.Repositories.Interfaces;
+using vivuvn_api.Services.Interfaces;
 
 namespace vivuvn_api.Services.Implementations
 {
-    public class ItineraryHotelService : IitineraryHotelService
+    public class ItineraryHotelService(IUnitOfWork _unitOfWork, IMapper _mapper) : IitineraryHotelService
     {
+        public async Task<IEnumerable<ItineraryHotelDto>> GetHotelsInItineraryAsync(int itineraryId)
+        {
+            var itineraryHotels = await _unitOfWork.ItineraryHotels.GetAllAsync(ir => ir.ItineraryId == itineraryId, includeProperties: "Hotel,BudgetItem");
+            var itineraryHotelDtos = _mapper.Map<IEnumerable<ItineraryHotelDto>>(itineraryHotels);
+            return itineraryHotelDtos;
+        }
     }
 }
