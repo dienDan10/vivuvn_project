471c15caf209963ef9c6ba6b426619cb75f02e64
diendanx <urekkmazinno@gmail.com>
2025-11-07 19:38:42 +0700

    add json converter to request to advoid case sensitive

--- vivuvn_api/vivuvn_api/DTOs/Request/UpdateItineraryTransportationRequestDto.cs
+++ vivuvn_api/vivuvn_api/DTOs/Request/UpdateItineraryTransportationRequestDto.cs
@@ -1,7 +1,10 @@
-﻿namespace vivuvn_api.DTOs.Request
+﻿using System.Text.Json.Serialization;
+
+namespace vivuvn_api.DTOs.Request
 {
     public class UpdateItineraryTransportationRequestDto
     {
+        [JsonConverter(typeof(JsonStringEnumConverter))]
         public TransportationMode Transportation { get; set; }
     }
 
