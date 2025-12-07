5d93f2845ecd3e0b6575c9b7f009176aecc0b6cd
diendanx <urekkmazinno@gmail.com>
2025-12-07 17:42:59 +0700

    refactor generate schedule code in back end api

---
+++ vivuvn_api/vivuvn_api/DTOs/Response/AutoGenerateItineraryResponseDto.cs
@@ -0,0 +1,10 @@
+﻿namespace vivuvn_api.DTOs.Response
+{
+    public class AutoGenerateItineraryResponseDto
+    {
+        public string Status { get; set; } = string.Empty;
+        public string Message { get; set; } = string.Empty;
+        public int ItineraryId { get; set; }
+        public string Warning { get; set; } = string.Empty;
+    }
+}
