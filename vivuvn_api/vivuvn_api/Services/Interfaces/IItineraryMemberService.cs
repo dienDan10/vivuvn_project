1d2b7a78f0ff1a87d562f22ac369845172008de9
diendanx <urekkmazinno@gmail.com>
2025-12-08 23:06:08 +0700

    add endpoint for join public itinerary

--- vivuvn_api/vivuvn_api/Services/Interfaces/IItineraryMemberService.cs
+++ vivuvn_api/vivuvn_api/Services/Interfaces/IItineraryMemberService.cs
@@ -6,12 +6,14 @@ namespace vivuvn_api.Services.Interfaces
     {
         Task<InviteCodeDto> GenerateInviteCodeAsync(int itineraryId, int ownerId);
         Task JoinItineraryByInviteCodeAsync(int userId, string inviteCode);
+        Task JoinPublicItineraryAsync(int userId, int itineraryId);
         Task<IEnumerable<ItineraryMemberDto>> GetMembersAsync(int itineraryId);
         Task LeaveItineraryAsync(int userId, int itineraryId);
         Task KickMemberAsync(int userId, int itineraryId, int memberId);
 
         // Helper methods
         Task<bool> IsOwnerAsync(int itineraryId, int userId);
+        // check if user is a member of the itinerary include owners
         Task<bool> IsMemberAsync(int itineraryId, int userId);
         Task<int> GetCurrentMemberCountAsync(int itineraryId);
     }
