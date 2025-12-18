4d3e29d05fbf4981e57afaac5802547354317c0b
haianh18 <laivuhaianh18@gmail.com>
2025-12-18 20:31:03 +0700

    add zoom in profile avatar

--- vivuvn_app/lib/features/profile/ui/widgets/profile_picture_container.dart
+++ vivuvn_app/lib/features/profile/ui/widgets/profile_picture_container.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import '../../controller/profile_controller.dart';
 import 'profile_picture_image.dart';
+import 'profile_picture_preview_dialog.dart';
 
 class ProfilePictureContainer extends ConsumerWidget {
   const ProfilePictureContainer({super.key});
@@ -10,21 +12,24 @@ class ProfilePictureContainer extends ConsumerWidget {
   Widget build(final BuildContext context, final WidgetRef ref) {
     final theme = Theme.of(context);
     final colorScheme = theme.colorScheme;
+    final user = ref.watch(
+      profileControllerProvider.select((final s) => s.user),
+    );
+    final hasPhoto = user?.userPhoto != null && user!.userPhoto!.isNotEmpty;
 
-    return Container(
-      width: 120,
-      height: 120,
-      decoration: BoxDecoration(
-        shape: BoxShape.circle,
-        border: Border.all(
-          color: colorScheme.primary,
-          width: 3,
+    return GestureDetector(
+      onTap: hasPhoto
+          ? () => ProfilePicturePreviewDialog.show(context, user.userPhoto!)
+          : null,
+      child: Container(
+        width: 120,
+        height: 120,
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          border: Border.all(color: colorScheme.primary, width: 3),
         ),
-      ),
-      child: const ClipOval(
-        child: ProfilePictureImage(),
+        child: const ClipOval(child: ProfilePictureImage()),
       ),
     );
   }
 }
-
